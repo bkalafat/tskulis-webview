044c79b3d1b3b46f182f64066c0d2565637d0605
bkalafat <kalafatburak@gmail.com>
2021-06-15 20:23:05 +0300

    feat: css

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import React, { useState, useRef } from 'react';
-import {Button, RefreshControl, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { StatusBar, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function App() {
-  const [refreshing, setRefreshing] = useState(false);
 
   const webViewRef = useRef(null)
 
@@ -11,37 +10,25 @@ export default function App() {
     webViewRef.current.goBack();
   };
 
-  const wait = (timeout) => {
-    return new Promise(resolve => setTimeout(resolve, timeout));
-  }
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    setSource({ uri: 'https://tskulis.com' })
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
-
-
-  const [source, setSource] = useState({ uri: 'https://tskulis.com' })
   return <SafeAreaView style={styles.container}>
-    <ScrollView
-      contentContainerStyle={styles.container}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-        />
-      }
-    >
-      <WebView ref={webViewRef}  source={source} />
-      <Button title="Geri Dön" onPress={goback}></Button>
-
-    </ScrollView>
+    <WebView ref={webViewRef} source={{ uri: 'https://tskulis.com' }} />
+    <TouchableOpacity  onPress={goback} on style={styles.button}>
+      <Text>geri</Text>
+    </TouchableOpacity>
   </SafeAreaView>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight
   },
-});
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width:40,
+    marginHorizontal:20,
+    borderRadius:9
+  },
+})
